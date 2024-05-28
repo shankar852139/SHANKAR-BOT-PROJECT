2266606e1758935ef260eff4306c8850c331956e
Alisha7295 <168325700+Alisha7295@users.noreply.github.com>
2024-05-28 22:19:20 +0530

    Update goibot.js

--- script/commands/goibot.js
+++ script/commands/goibot.js
@@ -82,7 +82,7 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
 
     setTimeout(function() {
       return api.sendMessage(msg, threadID, messageID);
-    }, 200);
+    }, 50);
   }
 
   if (
